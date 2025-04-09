d63fbb6dae29b7bfe3a3175ac3bea0cf54d13e03
Nolent77 <nolhan795@gmail.com>
2025-04-09 13:57:59 +0200

    add of admin directory and dashbord blade inside

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View; // to detect the vu (precise of type)
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(): View // to detect the vu (precise of type)
     {
         return view('admin.dashboard');
     }
